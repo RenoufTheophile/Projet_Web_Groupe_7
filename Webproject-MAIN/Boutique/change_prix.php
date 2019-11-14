7f014d90e6d25b9f6f7bfc278e4a5ac608b0bfc5
Antoine Cahard <antoine.cahard@gmail.com>
2019-11-14 10:12:28 +0100

    modification des  liens

--- Webproject-MAIN/Boutique/change_prix.php
+++ Webproject-MAIN/Boutique/change_prix.php
@@ -5,5 +5,5 @@ page associé aux input de prix
  <?php
 $prix_min=$_POST['prix_min'];
 $prix_max=$_POST['prix_max'];
-header('Location:Boutique_up.php?prix_min='.$prix_min.'&prix_max='.$prix_max);
+header('Location:../Boutique.php?prix_min='.$prix_min.'&prix_max='.$prix_max);
 ?>
