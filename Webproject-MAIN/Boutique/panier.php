63190201e37d1e59768d9b9c648d061416b023cb
Antoine Cahard <antoine.cahard@gmail.com>
2019-11-15 09:08:24 +0100

    debut panier

---
+++ Webproject-MAIN/Boutique/panier.php
@@ -0,0 +1,30 @@
+<!--####################################
+requete pour remplir template_panier
+ #######################################-->
+
+ <?php
+    $bdd = new PDO('mysql:host=localhost;dbname=webproject;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+    $requete =$bdd->prepare("CALL info_panier(1)");
+    /*$requete->bindParam(':id',$id);
+    $requete->execute();*/
+
+    $data=$requete->fetch(PDO::FETCH_ASSOC);
+        echo "<div class='produit_seul'>
+                <div class='flottant'>
+                    <img src='image_temp/".$data['goodies_photo']."' class='image_p' alt='photo produit'/>
+                </div>
+                <div class='info_p'>
+                    
+                    <div class='name'> <strong>".$data['goodies_name']."</strong></div>
+                    <div class='prix'> Prix: ".$data['goodies_cost']."€ </div>
+                    <div class='catégorie'> Catégorie: ".$data['goodies_category']."</div>
+                    <div class='description'> Description: </br>".$data['goodies_description']."</div>
+                </div>
+                   
+            </div>";
+    
+    
+    $requete->closeCursor();
+
+?>
