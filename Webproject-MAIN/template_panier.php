7a8c9bd1371430577c5d545072638e0b050e5f31
Antoine Cahard <antoine.cahard@gmail.com>
2019-11-18 10:26:29 +0100

    lien panier

--- Webproject-MAIN/template_panier.php
+++ Webproject-MAIN/template_panier.php
@@ -37,7 +37,7 @@ session_start();
 
     <!-- Corps -->
     <div id='validation'>
-        <form action='/webproject/Webproject-MAIN/Boutique/valide_panier.php' method='post' id='form_panier'>
+        <form action='Boutique/valide_panier.php' method='post' id='form_panier'>
             <input type='submit' name='validation' value='Passer commande'/>
         </form>
     </div>
